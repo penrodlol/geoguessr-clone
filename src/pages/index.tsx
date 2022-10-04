c421467d9ed265dcaa649f1f8e1647fffc4b1165
penrodlol <christian@penrod.contact>
2022-10-03 21:38:24 -0400

    ⚡️ Prevent flash of no buttons

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,21 +1,28 @@
 import { Button } from '@components/Button';
 import { Providers } from '@components/Providers';
-import { trpc } from '@utils/trpc';
-import type { NextPage } from 'next';
+import { getServerSession } from '@server/common/server-session';
+import type {
+  GetServerSideProps,
+  InferGetServerSidePropsType,
+  NextPage,
+} from 'next';
 
-const Home: NextPage = () => {
-  const { data: user, isLoading } = trpc.session.getUser.useQuery();
+type SSRPage = NextPage<InferGetServerSidePropsType<typeof getServerSideProps>>;
+type SSRProps = { loggedIn: boolean };
 
-  return (
-    <section className="flex flex-col items-center gap-2">
-      <h1 className="text-gradient fluid-5xl">GeoGuessr Clone</h1>
-      <p className="fluid-2xl">A clone of the popular game GeoGuessr.</p>
-      <div className="mt-20 flex flex-col gap-5">
-        {!isLoading &&
-          (user ? <Button className="fluid-xl">Play</Button> : <Providers />)}
-      </div>
-    </section>
-  );
+const Home: SSRPage = ({ loggedIn }) => (
+  <section className="flex flex-col items-center gap-2">
+    <h1 className="text-gradient fluid-5xl">GeoGuessr Clone</h1>
+    <p className="fluid-2xl">A clone of the popular game GeoGuessr.</p>
+    <div className="mt-20 flex flex-col gap-5">
+      {loggedIn ? <Button className="fluid-xl">Play</Button> : <Providers />}
+    </div>
+  </section>
+);
+
+export const getServerSideProps: GetServerSideProps<SSRProps> = async (ctx) => {
+  const session = await getServerSession({ req: ctx.req, res: ctx.res });
+  return { props: { loggedIn: !!session?.user } };
 };
 
 export default Home;
